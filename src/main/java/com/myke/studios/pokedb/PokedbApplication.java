f5ccd362625f543b1c33607e15b60691e85fb5ae
durielbaal <michaelogwu1997@gmail.com>
2024-10-14 09:58:49 +0200

    Initialized project

---
+++ src/main/java/com/myke/studios/pokedb/PokedbApplication.java
@@ -0,0 +1,13 @@
+package com.myke.studios.pokedb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokedbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokedbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myke/studios/pokedb/PokedbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myke.studios.pokedb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokedbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
